836afd2dbedd3a59a3468de025412eea28ff9dbb
Konrad Nowakowski <konrad.x92@gmail.com>
2021-04-14 18:08:25 +0100

    Add basic components

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue'
+import HomePage from './pages/HomePage'
+
+export default defineComponent({
+  setup() {
+    return () => <HomePage />
+  },
+})
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomePage',
+  setup() {
+    return () => <div>HomePage.tsx</div>
+  },
+})
